5a54e67d80858259793f0b970c227153db1f4c06
Animesh Kumar <animuz111@gmail.com>
2020-06-18 13:07:40 +0530

    filter-junit: Fix gofmt error
    Signed-off-by: Animesh Kumar <animuz111@gmail.com>

--- filter-junit.go
+++ filter-junit.go
@@ -18,7 +18,7 @@ limitations under the License.
 This command filters a JUnit file such that only tests with a name
 matching a regular expression are passed through. By concatenating
 multiple input files it is possible to merge them into a single file.
- */
+*/
 package main
 
 import (
